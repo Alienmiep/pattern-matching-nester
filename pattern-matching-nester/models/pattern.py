46b043547e7fd201eb49f3e9dfc855125673468f
Alienmiep <alienmiep@gmx.de>
2025-08-23 14:13:57 +0200

    Use vertex index instead of raw coordinates

--- pattern-matching-nester/models/pattern.py
+++ pattern-matching-nester/models/pattern.py
@@ -6,17 +6,14 @@ class Pattern():
     def __str__(self):
         return ";\n".join([str(x) for x in self.pieces]) if self.pieces else ""
 
-    def find_seams_by_pair(self, part_1_full_name: str, part_2_full_name: str) -> list:
-        part_1_names = part_1_full_name.split("+")
-        part_2_names = part_2_full_name.split("+")
-
+    def find_seams_by_pair(self, part_1_name: str, part_2_name: str) -> list:
         affected_seams = []
         for seam in self.seams:
             if len(seam.seamparts) != 2:
                 continue
+
             parts_in_seam = [x.part for x in seam.seamparts]
-            if (parts_in_seam[0] in part_1_names and parts_in_seam[1] in part_2_names) \
-                or (parts_in_seam[0] in part_2_names and parts_in_seam[1] in part_1_names):
+            if (part_1_name in parts_in_seam and part_2_name in parts_in_seam):
                 affected_seams.append(seam)
 
         return affected_seams
