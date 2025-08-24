f446c43943f2bb9c373aba5570146034a880fea3
Alienmiep <alienmiep@gmx.de>
2025-08-24 12:54:44 +0200

    Use reference point in IFP algorithm

--- pattern-matching-nester/ifp.py
+++ pattern-matching-nester/ifp.py
@@ -1,3 +1,5 @@
+from models.piece import Piece
+
 # input_points = [(14.2, 147.0), (14.2, 154.0), (0.0, 154.0), (-14.2, 154.0), (-14.2, 147.0), (0.0, 147.0)]
 input_points_local = [(180.1, 147.0), (170.3, 147.0), (160.5, 147.0), (160.5, 154.0), (170.3, 154.0), (180.1, 154.0)]
 
@@ -8,8 +10,9 @@ fabric_width = 150   # = y
 
 fabric_vertices = [(0, 0), (200, 0), (200, 150), (0, 150)]
 
-def ifp(input_points: list, fabric_vertices: list) -> list:
-    reference_point = min(input_points, key=lambda v: (v[0], v[1])) # TODO
+def ifp(piece: Piece, fabric_vertices: list) -> list:
+    input_points = piece.vertices
+    # reference_point = min(input_points, key=lambda v: (v[0], v[1]))
 
     # Transpose the list of tuples to separate x and y values
     x_vals, y_vals = zip(*input_points)
@@ -19,10 +22,10 @@ def ifp(input_points: list, fabric_vertices: list) -> list:
     max_x = max(x_vals)
     max_y = max(y_vals)  # to make axis-aligned bounding box of the piece
 
-    ifp_min_x = reference_point[0] - min_x
-    ifp_max_x = fabric_length - (max_x - reference_point[0])
-    ifp_min_y = reference_point[1] - min_y
-    ifp_max_y = fabric_width - (max_y - reference_point[1])
+    ifp_min_x = piece.reference_point[0] - min_x
+    ifp_max_x = fabric_length - (max_x - piece.reference_point[0])
+    ifp_min_y = piece.reference_point[1] - min_y
+    ifp_max_y = fabric_width - (max_y - piece.reference_point[1])
     print(ifp_min_x, ifp_max_x)
 
     ifp = [(ifp_min_x, ifp_min_y), (ifp_max_x, ifp_min_y), (ifp_max_x, ifp_max_y), (ifp_min_x, ifp_max_y)]
